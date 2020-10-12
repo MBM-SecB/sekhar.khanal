fdde24d1ef8f60c9a14397ee02a3b5c1c7421359
sekhar12322 <khanalsekhar13@gmail.com>
2020-10-12 11:55:27 +0545

    cw

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -6,16 +6,16 @@ public class EmployeeController : Controller
 {
     public ActionResult Index()
     {
-        // Object initializer
-        var employee1 = new Employee { FirstName = "Bishnu", Surname = "Rawal", Address = "Ktm", Salary = 100.0 };
-        var employee2 = new Employee { FirstName = "Ram", Surname = "Basnet", Address = "Bhaktapur", Salary = 10000.0 };
-        var employee3 = new Employee { FirstName = "KP", Surname = "Oli", Address = "Ktm", Salary = 100.0 };
-        var employee4 = new Employee { FirstName = "Bishnu", Surname = "Rawal", Address = "Ktm", Salary = 100.0 };
-        var employee5 = new Employee { FirstName = "Bishnu", Surname = "Rawal", Address = "Ktm", Salary = 100.0 };
-        var employee6 = new Employee { FirstName = "Bishnu", Surname = "Rawal", Address = "Ktm", Salary = 100.0 };
-
-        var employees = new List<Employee> { employee1, employee2, employee3, employee4, employee5, employee6 };
-        
+       var employees = Employee.GetEmployees();
         return View(employees);
     }
+
+    public ActionResult Detail(string firstName)
+    {
+        var employees = Employee.GetEmployees();
+        var employee = employees.Where(x => x.Firstname == firstName).First();
+        return View();
+    }
 }
+
+
--- EmployeeManagement/Models/Employee.cs
+++ EmployeeManagement/Models/Employee.cs
@@ -5,4 +5,23 @@ public class Employee
     public string Address { get; set; }
     public char Gender { get; set; } = 'M';
     public double Salary { get; set; }
+
+public List<Employee> GetEmployees()
+{
+     // Object initializer
+        var employee1 = new Employee { FirstName = "sekhar", Surname = "khanal", Address = "Ktm", Salary = 100.0 };
+        var employee2 = new Employee { FirstName = "Raman", Surname = "kumar", Address = "Bhaktapur", Salary = 10000.0 };
+        var employee3 = new Employee { FirstName = "sheru", Surname = "Oli", Address = "Ktm", Salary = 100.0 };
+        var employee4 = new Employee { FirstName = "sagar", Surname = "neupane", Address = "Ktm", Salary = 100.0 };
+        var employee5 = new Employee { FirstName = "Bishal", Surname = "timilsina", Address = "Ktm", Salary = 100.0 };
+        var employee6 = new Employee { FirstName = "nirmala", Surname = "chettri", Address = "Ktm", Salary = 100.0 };
+
+        var employees = new List<Employee> { employee1, employee2, employee3, employee4, employee5, employee6 };
+        return employees;
+}
+
+
+
+
+
 }
